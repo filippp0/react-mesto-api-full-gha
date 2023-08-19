f036ff42b43c06d1c3b7a7d9e526772702c4bc5c
Filipp Kolesnikov <filippp0@yandex.ru>
2023-08-20 01:16:09 +0300

    добавить краштест

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,17 @@ const signupRouter = require('./signup');
 const signinRouter = require('./singnin');
 const NotFoundError = require('../errors/NotFoundError');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 router.use('/signup', signupRouter);
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 router.use('/signin', signinRouter);
 // router.use(auth);
 router.use('/users', auth, usersRouter);
